1b9c7074d76d96cf277944fb40801818ef5b745b
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-05 17:17:27 +0200

    Add: controller

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+
+class PageController extends Controller
+{
+    public function index(){
+
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
+
+    }
+}
